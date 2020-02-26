248dfbcef0912a132865c8173102b89999d60654
ilya-unr <ikuznetsov@nevada.unr.edu>
2020-02-25 16:47:15 -0800

    Refractor home page to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -19,3 +20,26 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))  
         self.assertIn('<title>To-Do lists</title>', html)  
         self.assertTrue(html.endswith('</html>'))  
+        self.assertTrue(html.strip().endswith('</html>'))
+
+
+    def test_home_page_returns_correct_html(self):
+        # request = HttpRequest()
+        # response = home_page(request)
+        # html = response.content.decode('utf8')
+        # expected_html = render_to_string('home.html')
+        # self.assertEqual(html, expected_html)
+        response = self.client.get('/')  
+
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
+        #self.assertTemplateUsed(response, 'wrong.html')
+
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
